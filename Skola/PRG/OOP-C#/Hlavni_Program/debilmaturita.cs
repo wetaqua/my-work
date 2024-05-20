f45b4f00afcace334e9aa5986b14fbf5f89c4ec8
Marek Boruvka <fexfexYT@gmail.com>
2024-05-20 05:21:12 +0200

    20.05.2024

---
+++ Skola/PRG/OOP-C#/Hlavni_Program/debilmaturita.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hlavni_Program
+{
+    internal class Osoba
+    {
+        private string jmeno;
+        private string prijmeni;
+        private string vek;
+
+        public Osoba(string Jmeno, string prijmeni, string vek)
+        {
+            jmeno = Jmeno;
+            this.prijmeni = prijmeni;
+            this.vek = vek;
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine(jmeno + " " + prijmeni + " " + vek);
+        }
+    }
+}
